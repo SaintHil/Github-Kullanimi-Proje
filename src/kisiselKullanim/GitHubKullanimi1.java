ec31566ee508cd1110fdb41057320dd7f16f04ff
Your Name <you@example.com>
2023-06-05 15:31:46 +0200

    ïlk Degisikligi Yaptim

---
+++ src/kisiselKullanim/GitHubKullanimi1.java
@@ -0,0 +1,9 @@
+package kisiselKullanim;
+
+public class GitHubKullanimi1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(12>> 2);
+        System.out.println("Thats it");
+    }
+}
